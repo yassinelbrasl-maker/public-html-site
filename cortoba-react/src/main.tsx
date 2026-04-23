c04d0881f1faa6816c975fb25b2c8bd1f602d77f
yassinelbrasl-maker <yassine.lbr.asl@gmail.com>
2026-04-23 02:26:59 +0100

    Auto-commit changes

--- cortoba-react/src/main.tsx
+++ cortoba-react/src/main.tsx
@@ -4,13 +4,19 @@ import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { router } from "./router";
 import { I18nProvider } from "./i18n/I18nProvider";
+import { ToastProvider } from "./components/ToastProvider";
+import { ConfirmProvider } from "./components/ConfirmProvider";
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <I18nProvider>
-        <RouterProvider router={router} />
+        <ToastProvider>
+          <ConfirmProvider>
+            <RouterProvider router={router} />
+          </ConfirmProvider>
+        </ToastProvider>
       </I18nProvider>
     </HelmetProvider>
   </React.StrictMode>
